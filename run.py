178fbd4ee5b225d97fe5eb55658cff6e813aa0fa
MENDES DA SILVA Catia <cmendesdasilva@guardiaschool.fr>
2026-04-29 12:41:12 +0200

    bouton publier

--- run.py
+++ run.py
@@ -8,7 +8,4 @@ if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
 
     # Le "# nosec B104" dit à Bandit d'ignorer l'alerte sur l'adresse 0.0.0.0
-    app.run(debug=debug_mode, host='0.0.0.0', port=8000)  
-
-
-    buuuuuggg
+    app.run(debug=debug_mode, host='0.0.0.0', port=8000)
